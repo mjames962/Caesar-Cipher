91403211023602e51e4110e3075bdff266e85143
Mitch <mjames962@hotmail.com>
2020-10-20 11:45:23 +0100

    added decryption methods

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -7,7 +7,7 @@ public class CaesarCipher {
      * @throws IllegalArgumentException when input isn't A-Z
      * @return Ciphertext
      **/
-    public static char caesarCipher(char plaintext, int key) throws IllegalArgumentException{
+    public static char encrypt(char plaintext, int key) throws IllegalArgumentException{
         //checks to see if character input is valid e.g capital letter A-Z
         if (!(65 <= plaintext && plaintext <= 90)) {
             throw new IllegalArgumentException("Input is not capital letter");
@@ -30,18 +30,63 @@ public class CaesarCipher {
      * @param key the value the character will be shifted by e.g key = 3 then A becomes D
      * @return Ciphertext
      **/
-    public static String caesarCipher(String plaintext, int key) {
+    public static String encrypt(String plaintext, int key) {
         String cipherText = "";
 
         //stepping through each character in string
         for (int i = 0; i < plaintext.length(); i++) {
             try {
-                cipherText = cipherText + caesarCipher(plaintext.charAt(i), key);
+                cipherText = cipherText + encrypt(plaintext.charAt(i), key);
             } catch (IllegalArgumentException e) { //anything not a capital letter
                 cipherText = cipherText + " ";
             }
         }
 
         return cipherText;
+    }
+
+    /**
+     * Caesar cipher decryption for a single character.
+     *
+     * @param cipherText to be decrypted
+     * @param key used to encrypt the message
+     * @return plaintext character
+     * @throws IllegalArgumentException when input isn't A-Z
+     */
+    public static char decrypt(char cipherText, int key) throws IllegalArgumentException{
+        //checks to see if character input is valid e.g capital letter A-Z
+        if (!(65 <= cipherText && cipherText <= 90)) {
+            throw new IllegalArgumentException("Input is not capital letter");
+        }
+
+        //check to see if result is in range
+        if (cipherText - key >= 65) {
+            return (char) (cipherText - key);
+        } else { //wrap the character back around
+            return (char) (cipherText - key + 26);
+        }
+
+    }
+
+    /**
+     * Caesar cipher decryption for strings.
+     *
+     * @param cipherText to be decrypted
+     * @param key used to encrypt the message
+     * @return plaintext string
+     */
+    public static String decrypt(String cipherText, int key) {
+        String plainText = "";
+
+        //stepping through each character in string
+        for (int i = 0; i < cipherText.length(); i++) {
+            try {
+                plainText = plainText + decrypt(cipherText.charAt(i), key);
+            } catch (IllegalArgumentException e) { //anything not a capital letter
+                plainText = plainText + " ";
+            }
+        }
+
+        return plainText;
     }
 }
